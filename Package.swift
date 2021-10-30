1da2cd61e345ec75aad54f36705ec0cbeee2d140
Bobbie Markwick <rnmarkwick@gmail.com>
2021-10-29 22:23:10 -0400

    Rename

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 import SwiftUI
 
 let package = Package(
-    name: "RMSwiftUIX",
+    name: "SwiftUIX",
     platforms: [
         .iOS(.v14),
         .macOS(.v11),
@@ -15,8 +15,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "RMSwiftUIX",
-            targets: ["RMSwiftUIX"]),
+            name: "SwiftUIX",
+            targets: ["SwiftUIX"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -26,7 +26,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "RMSwiftUIX",
+            name: "SwiftUIX",
             dependencies: []),
     ]
 )
