367594bb98953d7599c8e40116556758f6e880b1
Bobbie Markwick <rnmarkwick@gmail.com>
2021-11-02 00:04:41 -0400

    Window fix

--- Sources/SwiftUIX/Window.swift
+++ Sources/SwiftUIX/Window.swift
@@ -15,9 +15,9 @@ public extension View {
 }
 #if os (macOS)
 struct Window<Root:View>:ViewModifier{
-     var isPresented:Binding<Bool>
+    @Binding var isPresented:Bool
     var content: ()->Root
-    func body(content: Content) -> some View { HStack{ content; MakerRep(isPresented: isPresented, content: self.content) } }
+    func body(content: Content) -> some View { HStack{ content; MakerRep(isPresented: $isPresented, content: self.content) } }
 }
 
 struct MakerRep<Root:View>:NSViewControllerRepresentable {
@@ -43,93 +43,6 @@ struct MakerRep<Root:View>:NSViewControllerRepresentable {
         else { window?.close(); window = nil }
     }
 }
-
-
-
-
-/*
-
-class WindowWrapper:ObservableObject{
-    var id:String
-    var shouldPresent:Binding<Bool> {
-        didSet{
-            if shouldPresent.wrappedValue && state == .inactive {
-                state = .pending
-                if let url = URL(string: "stdcrd://ShowWindow") {
-                    NSWorkspace.shared.open(url)
-                }
-            } else if shouldPresent.wrappedValue{
-                if state == .presented || state == .pending { dismissFunction() }
-            }
-        }
-    }
-    
-    enum WindowState { case pending, presented, dismissing, inactive }
-    var state:WindowState
-    var content:()->AnyView
-    
-    var dismissFunction:()->() = {}
-    func dismiss(){
-        state = .pending
-        shouldPresent.wrappedValue = false
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.1){ self.state = .inactive }
-    }
-    
-    init(isPresented:Binding<Bool>, identifier:String, @ViewBuilder content:@escaping ()->AnyView){
-        self.id = identifier
-        self.content = content
-        self.state = .inactive
-        self.shouldPresent = isPresented
-    }
-    
-    func load()->some View {
-        if self.id == "PLACEHOLDER",  let wrapper = windowWrappers.first(where: {$0.state == .pending}) {
-            if let idx = windowWrappers.firstIndex(where:  {$0.state == .pending}) {
-                windowWrappers[idx] = self
-            }
-            self.id = wrapper.id
-            self.state = wrapper.state
-            self.shouldPresent = wrapper.shouldPresent
-            self.content = wrapper.content
-        }
-        return content()
-    }
-}
-
-@available (macOS 12, *)
-public var wrappedWindows:some Scene {
-    return WindowGroup{
-        ContainerView()
-    }.handlesExternalEvents(matching: Set(arrayLiteral: "ShowWindow"))
-}
-var windowWrappers:[WindowWrapper] = []
-
-
-@available (macOS 12, *)
-struct ContainerView:View{
-    var maker:WindowWrapper
-    
-    @Environment(\.dismiss) var dismiss
-
-    init(){
-        if let wrapper = windowWrappers.first(where: {$0.state == .pending}) {
-            maker = wrapper
-        } else {
-            self.maker = WindowWrapper(isPresented:  Binding<Bool>.init(get: {false}, set: {_ in }), identifier: "PLACEHOLDER"){  AnyView(EmptyView())  }
-        }
-        maker.dismissFunction = dismiss.callAsFunction
-    }
-    
-    var body: some View{
-        maker.load().onAppear{
-            maker.state = .presented
-            if maker.id == "PLACEHOLDER" {  dismiss() }
-        }.onDisappear{ maker.dismiss()  }
-    }
-}
-
-
-*/
 
 #else
 //Default to a full screen cover on other platforms
