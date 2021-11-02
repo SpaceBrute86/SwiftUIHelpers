14127671ecc15dbb7b5c3f759d321261818c7820
Bobbie Markwick <rnmarkwick@gmail.com>
2021-11-02 00:03:11 -0400

    Window without weird glitches, in exchange for dismissibility

--- Sources/SwiftUIX/Window.swift
+++ Sources/SwiftUIX/Window.swift
@@ -10,25 +10,44 @@ import SwiftUI
 @available (iOS 14, macOS 12, *)
 public extension View {
     func window<Root:View>(isPresented:Binding<Bool>, @ViewBuilder content:@escaping ()->Root) -> some View {
-        modifier(Window(isPresented: isPresented, identifier:"Test", content: content))
+        modifier(Window(isPresented: isPresented, content: content))
     }
 }
 #if os (macOS)
 struct Window<Root:View>:ViewModifier{
-    var maker:WindowWrapper
-    init(isPresented:Binding<Bool>, identifier:String, @ViewBuilder content:@escaping ()->Root){
-        if let m = windowWrappers.first(where: {$0.id == identifier}) {
-            maker = m
-            if maker.state != .presented { maker.content = { AnyView(content())  } }
-            maker.shouldPresent = isPresented
-        }
-        else {
-            maker = WindowWrapper(isPresented: isPresented, identifier: identifier){ AnyView(content())  }
-            windowWrappers += [maker]
-        }
-    }
-    func body(content: Content) -> some View { content  }
+     var isPresented:Binding<Bool>
+    var content: ()->Root
+    func body(content: Content) -> some View { HStack{ content; MakerRep(isPresented: isPresented, content: self.content) } }
 }
+
+struct MakerRep<Root:View>:NSViewControllerRepresentable {
+    @Binding var isPresented:Bool
+    var content: ()->Root
+    @State var window:NSWindow?
+    
+    func showWindow(){
+        guard window == nil else { return }
+        window = NSWindow(contentViewController: NSHostingController(rootView: content()))
+        window!.center()
+        window!.isReleasedWhenClosed = false
+        window!.makeKeyAndOrderFront(nil)
+    }
+    func makeNSViewController(context: Context) -> NSViewController {
+        if isPresented { showWindow() }
+        else { window?.close(); window = nil }
+        return NSViewController()
+    }
+    func updateNSViewController(_ nsViewController: NSViewController, context: Context) {
+        nsViewController.view.frame.size = CGSize.zero
+        if isPresented { showWindow() }
+        else { window?.close(); window = nil }
+    }
+}
+
+
+
+
+/*
 
 class WindowWrapper:ObservableObject{
     var id:String
@@ -110,11 +129,12 @@ struct ContainerView:View{
 }
 
 
+*/
 
 #else
 //Default to a full screen cover on other platforms
 struct Window<Root:View>:ViewModifier{
-    var isPresented:Binding<Bool>, identifier:String, content:()->Root
+    var isPresented:Binding<Bool>, content:()->Root
     func body(content: Content) -> some View { content.fullScreenCover(isPresented: isPresented, onDismiss: nil, content: self.content) }
 }
 #endif
